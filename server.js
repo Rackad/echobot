edfc0ff8f079cbcd506a9c015d83c67b7c5958e2
bs <bs@rackads-MacBook-Pro.local>
2017-05-11 05:34:49 +0300

    nothing

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ var restify = require('restify');
 var builder = require('botbuilder');
 
 // Get secrets from server environment
-var botConnectorOptions = { 
-    appId: process.env.BOTFRAMEWORK_APPID, 
+var botConnectorOptions = {
+    appId: process.env.BOTFRAMEWORK_APPID,
     appPassword: process.env.BOTFRAMEWORK_APPSECRET
 };
 
@@ -11,12 +11,35 @@ var botConnectorOptions = {
 var connector = new builder.ChatConnector(botConnectorOptions);
 var bot = new builder.UniversalBot(connector);
 
-bot.dialog('/', function (session) {
-    
-    //respond with user's message
-    session.send("You said " + session.message.text);
-});
 
+bot.dialog('/', [
+  /*function(session){
+    builder.Prompts.text(session,'مرحبا بك في متجر ');
+  },*/
+  function (session) {
+    builder.Prompts.choice(session, "بأيش أقدر أخدمك:", 'طلب جديد| حالة الطلب | التواصل مع البائع | إنهاء المحادثة ');
+  },
+  function (session, results) {
+
+            switch (results.response.index) {
+                case 0:
+                    session.send('اطلب');
+                    break;
+                case 1:
+
+                    session.send('حالة الطلب');
+                    break;
+                case 2:
+                    session.send('الايميل');
+                    break;
+                default:
+                    session.endDialog();
+                    break;
+            }
+
+
+ } ]);
+ 
 // Setup Restify Server
 var server = restify.createServer();
 
@@ -30,5 +53,5 @@ server.get(/.*/, restify.serveStatic({
 }));
 
 server.listen(process.env.port || 3978, function () {
-    console.log('%s listening to %s', server.name, server.url); 
+    console.log('%s listening to %s', server.name, server.url);
 });
